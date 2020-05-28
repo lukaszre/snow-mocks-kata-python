581e26e587556dcef99ae96cc0da52196083281b
michal.lipski <kiwaczki@gmail.com>
2020-05-28 18:41:54 +0200

    updated readme

--- test/pragmatists/snowmocks/SnowRescueServiceTest.py
+++ test/pragmatists/snowmocks/SnowRescueServiceTest.py
@@ -1,10 +1,11 @@
 import unittest
 
-
+#A testcase is created by subclassing unittest.TestCase.
 class SnowRescueServiceTest(unittest.TestCase):
 
+    #tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.
     def test_start_here(self):
-        pass
+        self.assertEqual('foo'.upper(), 'FOO')
 
 if __name__ == '__main__':
     unittest.main()
