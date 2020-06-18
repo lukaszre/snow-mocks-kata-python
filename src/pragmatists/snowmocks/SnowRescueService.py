8f69a9f9dc51d2e5d5aac33645b9ddbcf44cef8f
lreszk01 <lukasz.reszka@orange.com>
2020-06-18 15:18:56 +0200

    Snow resource service is ready.

--- src/pragmatists/snowmocks/SnowRescueService.py
+++ src/pragmatists/snowmocks/SnowRescueService.py
@@ -1,4 +1,26 @@
+from src.pragmatists.snowmocks.dependencies.SnowplowMalfunctioningException import SnowplowMulfunctioningException
 
 
 class SnowRescueService(object):
-    pass
+    def __init__(self, municipal_service, weather_forecast_service, press_services):
+        self.municipal_service = municipal_service
+        self.weather_forecast_service = weather_forecast_service
+        self.press_services = press_services
+
+    def check_forecast_and_rescue(self):
+        if self.weather_forecast_service.get_average_temperature_in_celsius() < 0:
+            self.municipal_service.send_sander()
+        if self.weather_forecast_service.get_snow_fall_height_in_mm() >= 3:
+            self.send_snowplow()
+            if self.weather_forecast_service.get_snow_fall_height_in_mm() >= 5:
+                self.send_snowplow()
+                if self.weather_forecast_service.get_snow_fall_height_in_mm() >= 10:
+                    self.send_snowplow()
+                    self.press_services.send_weather_alert()
+
+    def send_snowplow(self):
+        try:
+            self.municipal_service.send_snowplow()
+        except SnowplowMulfunctioningException:
+            self.municipal_service.send_snowplow()
+            pass
--- test/pragmatists/snowmocks/SnowRescueServiceTest.py
+++ test/pragmatists/snowmocks/SnowRescueServiceTest.py
@@ -1,11 +1,81 @@
 import unittest
 
-#A testcase is created by subclassing unittest.TestCase.
+
+from unittest.mock import Mock
+
+from src.pragmatists.snowmocks.SnowRescueService import SnowRescueService
+from src.pragmatists.snowmocks.dependencies.PressService import PressServices
+from src.pragmatists.snowmocks.dependencies.MunicipalServices import MunicipalServices
+from src.pragmatists.snowmocks.dependencies.SnowplowMalfunctioningException import SnowplowMulfunctioningException
+from src.pragmatists.snowmocks.dependencies.WeatherForecastService import WeatherForecastService
+
+
 class SnowRescueServiceTest(unittest.TestCase):
 
-    #tests are defined with methods whose names start with the letters test. This naming convention informs the test runner about which methods represent tests.
-    def test_start_here(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def setUp(self):
+        self.weather_forecast_service = Mock(spec=WeatherForecastService)
+        self.municipal_services = Mock(spec=MunicipalServices)
+        self.press_services = Mock(spec=PressServices)
+
+        self.weather_forecast_service.get_average_temperature_in_celsius.return_value = 1
+        self.weather_forecast_service.get_snow_fall_height_in_mm.return_value = 0
+
+        self.snow_resource_service = SnowRescueService(self.municipal_services, self.weather_forecast_service,
+                                                       self.press_services)
+
+    def test_send_sander_when_temperature_is_below_0(self):
+        self.weather_forecast_service.get_average_temperature_in_celsius.return_value = -1
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.municipal_services.send_sander.assert_called()
+
+    def test_do_not_send_sander_when_temperature_is_greater_or_equal_0(self):
+        self.weather_forecast_service.get_average_temperature_in_celsius.return_value = 1
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.municipal_services.send_sander.assert_not_called()
+
+    def test_send_snowplow_when_snowfall_exceed_3_mm(self):
+        self.weather_forecast_service.get_snow_fall_height_in_mm.return_value = 3
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.municipal_services.send_snowplow.assert_called_once()
+
+    def test_do_not_send_snowplow_when_snowfall_do_not_exceed_3_mm(self):
+        self.weather_forecast_service.get_snow_fall_height_in_mm.return_value = 0
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.municipal_services.send_snowplow.assert_not_called()
+
+    def test_send_another_snowplow_when_first_will_fail(self):
+        self.weather_forecast_service.get_snow_fall_height_in_mm.return_value = 3
+        self.municipal_services.send_snowplow.side_effect = [SnowplowMulfunctioningException, None]
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.assertEqual(2, self.municipal_services.send_snowplow.call_count)
+
+    def test_send_two_snowplows_when_snowfall_exceed_5_mm(self):
+        self.weather_forecast_service.get_snow_fall_height_in_mm.return_value = 5
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.assertEqual(2, self.municipal_services.send_snowplow.call_count)
+
+    def test_send_3_snowplows_sander_and_notify_press_when_snowfall_exceed_10_mm_and_temperature_exceed_minus10(self):
+        self.weather_forecast_service.get_snow_fall_height_in_mm.return_value = 10
+        self.weather_forecast_service.get_average_temperature_in_celsius.return_value = -10
+
+        self.snow_resource_service.check_forecast_and_rescue()
+
+        self.assertEqual(3, self.municipal_services.send_snowplow.call_count)
+        self.municipal_services.send_sander.assert_called()
+        self.press_services.send_weather_alert.assert_called()
+
 
 if __name__ == '__main__':
     unittest.main()
